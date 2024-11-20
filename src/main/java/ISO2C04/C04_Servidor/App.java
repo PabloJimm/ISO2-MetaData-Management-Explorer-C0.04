755fdfe74a7c29180693ae3eb7a2b286642f92e6
PabloJimm <Pablo.Jimenez23@alu.uclm.es>
2024-11-20 21:15:19 +0100

    It2

---
+++ src/main/java/ISO2C04/C04_Servidor/App.java
@@ -0,0 +1,10 @@
+package ISO2C04.C04_Servidor;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/ISO2C04/C04_Servidor/AppTest.java
@@ -0,0 +1,19 @@
+package ISO2C04.C04_Servidor;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
